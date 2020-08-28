811eeb2716f5ba2f06d0ed7805ba7068dfde0d36
Saltysam101 <55891755+Saltysam101@users.noreply.github.com>
2020-08-28 10:48:56 -0400

    altered layout for desktop and adjusted forecast for future days

--- index.js
+++ index.js
@@ -60,34 +60,16 @@ window.addEventListener('load', () => {
                     })
                     .then(data => {
                         let newDate = new Date;
-                        let day = newDate.getDate();
-                        let month = newDate.getMonth() + 1;
-                        forecastDate.textContent = month + '/' + day;
-                        let iconcode = data.list[0].weather[0].icon;
-                        let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-                        forecastImg.setAttribute("src", iconurl);
-                        forecastDescription.textContent = data.list[0].weather[0].description;
-                        maxTemp.textContent = 'Max temp: ' + Math.round(data.list[0].main.temp_max) + ' F';
-                        realFeel.textContent = 'Real feel: ' + Math.round(data.list[0].main.feels_like) + ' F';
-                        minTemp.textContent = 'Min temp: ' + Math.round(data.list[0].main.temp_min) + ' F';
-                    })
-
-                    fetch(forecastApi)
-                    .then(response => {
-                        return response.json();
-                    })
-                    .then(data => {
-                        let newDate = new Date;
                         let day = newDate.getDate() + 1;
                         let month = newDate.getMonth() + 1;
-                        tomForecastDate.textContent = month + '/' + day;
+                        forecastDate.textContent = month + '/' + day;
                         let iconcode = data.list[8].weather[0].icon;
                         let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-                        tomForecastImg.setAttribute("src", iconurl);
-                        tomForecastDescription.textContent = data.list[8].weather[0].description;
-                        tomMaxTemp.textContent = 'Max temp: ' + Math.round(data.list[8].main.temp_max) + ' F';
-                        tomRealFeel.textContent = 'Real feel: ' + Math.round(data.list[8].main.feels_like) + ' F';
-                        tomMinTemp.textContent = 'Min temp: ' + Math.round(data.list[8].main.temp_min) + ' F';
+                        forecastImg.setAttribute("src", iconurl);
+                        forecastDescription.textContent = data.list[8].weather[0].description;
+                        maxTemp.textContent = 'Max temp: ' + Math.round(data.list[8].main.temp_max) + ' F';
+                        realFeel.textContent = 'Real feel: ' + Math.round(data.list[8].main.feels_like) + ' F';
+                        minTemp.textContent = 'Min temp: ' + Math.round(data.list[8].main.temp_min) + ' F';
                     })
 
                     fetch(forecastApi)
@@ -98,14 +80,32 @@ window.addEventListener('load', () => {
                         let newDate = new Date;
                         let day = newDate.getDate() + 2;
                         let month = newDate.getMonth() + 1;
+                        tomForecastDate.textContent = month + '/' + day;
+                        let iconcode = data.list[16].weather[0].icon;
+                        let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+                        tomForecastImg.setAttribute("src", iconurl);
+                        tomForecastDescription.textContent = data.list[16].weather[0].description;
+                        tomMaxTemp.textContent = 'Max temp: ' + Math.round(data.list[16].main.temp_max) + ' F';
+                        tomRealFeel.textContent = 'Real feel: ' + Math.round(data.list[16].main.feels_like) + ' F';
+                        tomMinTemp.textContent = 'Min temp: ' + Math.round(data.list[16].main.temp_min) + ' F';
+                    })
+
+                    fetch(forecastApi)
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        let newDate = new Date;
+                        let day = newDate.getDate() + 3;
+                        let month = newDate.getMonth() + 1;
                         datForecastDate.textContent = month + '/' + day;
                         let iconcode = data.list[16].weather[0].icon;
                         let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
                         datForecastImg.setAttribute("src", iconurl);
-                        datForecastDescription.textContent = data.list[16].weather[0].description;
-                        datMaxTemp.textContent = 'Max temp: ' + Math.round(data.list[16].main.temp_max) + ' F';
-                        datRealFeel.textContent = 'Real feel: ' + Math.round(data.list[16].main.feels_like) + ' F';
-                        datMinTemp.textContent = 'Min temp: ' + Math.round(data.list[16].main.temp_min) + ' F';
+                        datForecastDescription.textContent = data.list[24].weather[0].description;
+                        datMaxTemp.textContent = 'Max temp: ' + Math.round(data.list[24].main.temp_max) + ' F';
+                        datRealFeel.textContent = 'Real feel: ' + Math.round(data.list[24].main.feels_like) + ' F';
+                        datMinTemp.textContent = 'Min temp: ' + Math.round(data.list[24].main.temp_min) + ' F';
                     })
         })
     }
