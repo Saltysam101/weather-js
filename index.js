aa58a7908942c2b4c2e0e2a034f9a825af4e48b8
Saltysam101 <55891755+Saltysam101@users.noreply.github.com>
2020-11-09 10:16:10 -0500

    updated style of site, removed some code

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 window.addEventListener('load', () => {
+    alert("Please turn on location for this website to work")
     let long;
     let lat;
+    let randomMessage = document.querySelector('.random-message');
+    let currentContainer = document.querySelector('.current-weather');
     let location = document.querySelector('.location');
     let temp = document.querySelector('.temperature');
     let summary = document.querySelector('.summary');
-    let  img = document.getElementById('wicon');
+    let img = document.getElementById('wicon');
     let body = document.querySelector('.body');
+    let forecastContainer = document.querySelector('.forecast-container');
     let forecast = document.querySelector('.forecast');
     let forecastDate = document.querySelector('.date');
     let forecastImg = document.querySelector('.forecast-img');
     let forecastDescription = document.querySelector('.forecast-description');
-    let maxTemp = document.querySelector('.max-temp');
-    let minTemp = document.querySelector('.min-temp');
     let realFeel = document.querySelector('.feels-like');
     let tomForecastDate = document.querySelector('.tom-date');
     let tomForecastImg = document.querySelector('.tom-forecast-img');
     let tomForecastDescription = document.querySelector('.tom-forecast-description');
-    let tomMaxTemp = document.querySelector('.tom-max-temp');
-    let tomMinTemp = document.querySelector('.tom-min-temp');
     let tomRealFeel = document.querySelector('.tom-feels-like');
     let datForecastDate = document.querySelector('.dat-date');
     let datForecastImg = document.querySelector('.dat-forecast-img');
     let datForecastDescription = document.querySelector('.dat-forecast-description');
-    let datMaxTemp = document.querySelector('.dat-max-temp');
-    let datMinTemp = document.querySelector('.dat-min-temp');
     let datRealFeel = document.querySelector('.dat-feels-like');
     
     if(navigator.geolocation){
@@ -47,8 +45,11 @@ window.addEventListener('load', () => {
                     let iconcode = data.weather[0].icon;
                     let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
                     img.setAttribute("src", iconurl)
-                    if(data.main.temp >= 85){
+                    if(data.main.temp >= 80){
                         body.classList.toggle('warm')
+                        currentContainer.classList.toggle('warm')
+                        randomMessage.classList.toggle('warm')
+                        forecastContainer.classList.toggle('warm')
                     }
 
                     
@@ -67,9 +68,7 @@ window.addEventListener('load', () => {
                         let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
                         forecastImg.setAttribute("src", iconurl);
                         forecastDescription.textContent = data.list[8].weather[0].description;
-                        maxTemp.textContent = 'Max temp: ' + Math.round(data.list[8].main.temp_max) + ' F';
-                        realFeel.textContent = 'Real feel: ' + Math.round(data.list[8].main.feels_like) + ' F';
-                        minTemp.textContent = 'Min temp: ' + Math.round(data.list[8].main.temp_min) + ' F';
+                        realFeel.textContent = Math.round(data.list[8].main.feels_like) + ' F';
                     })
 
                     fetch(forecastApi)
@@ -85,9 +84,7 @@ window.addEventListener('load', () => {
                         let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
                         tomForecastImg.setAttribute("src", iconurl);
                         tomForecastDescription.textContent = data.list[16].weather[0].description;
-                        tomMaxTemp.textContent = 'Max temp: ' + Math.round(data.list[16].main.temp_max) + ' F';
-                        tomRealFeel.textContent = 'Real feel: ' + Math.round(data.list[16].main.feels_like) + ' F';
-                        tomMinTemp.textContent = 'Min temp: ' + Math.round(data.list[16].main.temp_min) + ' F';
+                        tomRealFeel.textContent = Math.round(data.list[16].main.feels_like) + ' F';
                     })
 
                     fetch(forecastApi)
@@ -103,9 +100,7 @@ window.addEventListener('load', () => {
                         let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
                         datForecastImg.setAttribute("src", iconurl);
                         datForecastDescription.textContent = data.list[24].weather[0].description;
-                        datMaxTemp.textContent = 'Max temp: ' + Math.round(data.list[24].main.temp_max) + ' F';
-                        datRealFeel.textContent = 'Real feel: ' + Math.round(data.list[24].main.feels_like) + ' F';
-                        datMinTemp.textContent = 'Min temp: ' + Math.round(data.list[24].main.temp_min) + ' F';
+                        datRealFeel.textContent = Math.round(data.list[24].main.feels_like) + ' F';
                     })
         })
     }
